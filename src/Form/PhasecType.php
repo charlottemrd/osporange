0dac68a6de037a09c09bb64a1a94a38b0859b7ba
charlottemrd <charlotte@milard.fr>
2021-11-03 10:58:12 +0100

    parametrer 60 80 100 ok pour change phase

--- src/Form/PhasecType.php
+++ src/Form/PhasecType.php
@@ -72,6 +72,13 @@ class PhasecType extends AbstractType
             ->add('garanti',IntegerType::class,['required'=>true], ['attr' => [
                 'class' => 'phasec_garanti']])
 
+            ->add('debit1bm',IntegerType::class,['required'=>false,], ['attr' => [
+                'class' => 'phasec_debit1bm']])
+            ->add('debit2bm',IntegerType::class,['required'=>false], ['attr' => [
+                'class' => 'phasec_debit2bm']])
+            ->add('debit3bm',IntegerType::class,['required'=>false], ['attr' => [
+                'class' => 'phasec_debit3bm']])
+
 
             //->add('DateLone')
 
